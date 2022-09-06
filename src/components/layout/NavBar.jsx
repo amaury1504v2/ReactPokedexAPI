3aa6c26c43e9dd50a3ffb422d5ab751165b77aca
Amaury1504v2 <57793398+amaury1504v2@users.noreply.github.com>
2022-09-06 03:59:37 +0200

    Logo

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+
 import './navbar.css';
+import logo from './pokedex.png';
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-            <a href="#" className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center" style={{marginLeft: "20px"}}>Pokedex</a>
+            <a href="#" className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center" style={{marginLeft: "20px"}}><img src={logo} style={{ height: '40px' }} alt="logo" /></a>
         </nav>
       </div>
     )
